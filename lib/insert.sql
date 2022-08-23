dc946110d18979d7af28832139bc2cad20e1776b
Whitney Muchira <whitneymuchira@gmail.com>
2022-08-23 15:21:58 +0300

    commit tree f2ffc165f6988ede9b605de88782923ec2037da4

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES 
+  (1,"Mr.chocolate", 4, "M","blue","calm",1),
+  (2,"Rowdy", 3, "M", "peach", "aggresive", 0),
+  (3,"Tabitha", 9, "F", "beige","introverted", 1),
+  (4,"Sergeant Brown", 6, "M", "pink", "extroverted",1),
+  (5,"Melissa", 7, "F", "yellow", "sigma", 1),
+  (6, "Grinch", 2, "M", "red", "quiet",1),
+  (7, "Wendy",8, "F", "purple","excited",1),
+  (8, null, 5,"M", "green", "happy", 1);
